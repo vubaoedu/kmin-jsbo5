54f2ae45f23c6c96ec0826fddbc35c4a15934af6
Dinh-Bao Vu <vubao.edu@gmail.com>
2021-12-08 21:10:09 +0700

    Add b01

---
+++ b01/code-reading/variables-primitive-data-types/01.js
@@ -0,0 +1,5 @@
+let x = 6;
+let y = 12;
+let z = 3;
+let t = x + y / z;
+console.log('t =', t);
---
+++ b01/code-reading/variables-primitive-data-types/02.js
@@ -0,0 +1,6 @@
+let x = 3;
+let y = 8;
+x = y + 1;
+y = x + y;
+console.log('x = ', x);
+console.log('y = ', y);
